85ed575419a31d3fe09ae69631397a20d189d12a
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-13 18:50:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require "pry"
+
+class School
+  ROSTER = {}
+  def initialize(school)
+    @school = school
+  end
+
+  def roster
+    @roster = ROSTER
+  end
+
+  def add_student(student, grade)
+    
+    if ROSTER.include?(grade)
+      ROSTER[grade] << student
+    else
+    ROSTER[grade] = []
+    ROSTER[grade] << student
+  end
+  end
+end
