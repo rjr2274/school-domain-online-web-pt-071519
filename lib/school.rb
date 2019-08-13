66240ca608414c2e991cb3480d4a688dffd48fae
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-13 19:21:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require "pry"
+
+class School
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map do |key, value|
+      value.sort!
+    end
+    @roster.sort.to_h
+  end
+
+end
